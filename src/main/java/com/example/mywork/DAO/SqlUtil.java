d43791872f84e789ff2f67141ff4273af0fa654f
Dinithi <nimashabvdinithi@gmail.com>
2025-02-10 14:33:28 +0530

    add sqlUtilClass

--- src/main/java/com/example/mywork/DAO/SqlUtil.java
+++ src/main/java/com/example/mywork/DAO/SqlUtil.java
@@ -1,4 +1,19 @@
 package com.example.mywork.DAO;
 
+import com.example.mywork.db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SqlUtil {
+    public static <T> T execute(String sql,Object... params) throws SQLException {
+        PreparedStatement preparedStatement = DBConnection.getInstance().getConnection().prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            preparedStatement.setObject((i + 1), params);
+        }
+        if (sql.startsWith("SELECT") ||sql.startsWith("select")){
+            return (T) preparedStatement.executeQuery();
+        }
+        return (T)(Boolean)(preparedStatement.executeUpdate() > 0);
+    }
 }
